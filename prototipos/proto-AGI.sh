6c542a0789fa68c15e9ff34f3011bb2c3e77c0f4
Pedro Mota <80173371+Pedro-02931@users.noreply.github.com>
2025-05-17 16:51:36 -0300

    Update proto-AGI.sh

--- prototipos/proto-AGI.sh
+++ prototipos/proto-AGI.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+cat << 'EOF' > "$APPLY_LOGIC_FILE"
+                    GNU GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
 
+ Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
+EOF
 BASE_DIR="/etc/bayes_mem"
 mkdir -p "$BASE_DIR"
 LOG_DIR="/var/log/bayes_mem"
